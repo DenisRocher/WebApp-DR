a14bb62732a227615448d3ec3311e28b6e25c8e9
Denis Rocher <denis.rocher@gmail.com>
2021-03-15 14:50:44 -0300

    --- Auto-Git Commit ---

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,23 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // Importar componentes
-import { AboutComponent } from './components/about'
-import { ContactComponent } from './components/contact';
-import { CreateComponent } from "./components/create";
-import { ProjectsComponent } from './home/home.component';
+import { AboutComponent } from './components/about/about.component'
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from "./components/create/create.component";
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ErrorComponent } from "./components/error/error.component";
 
 // Array de configuración de rutas
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'zapatillas', component: ZapatillasComponent },
-  { path: 'videojuego', component: VideojuegoComponent },
-  { path: 'cursos', component: CursosComponent },
-  { path: 'cursos/:nombre', component: CursosComponent },
-  { path: 'cursos/:nombre/:followers', component: CursosComponent },
-  { path: 'servicioexterno', component: ServicioexternoComponent},
-  { path: 'contacto', component: ContactoComponent},
-  { path: '**', component: HomeComponent }
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'sobre-mi', component: AboutComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: 'proyectos', component: ProjectsComponent },
+  { path: 'crear-proyecto', component: CreateComponent },
+  { path: '**', component: ErrorComponent }
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { CreateComponent } from './components/create/create.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactComponent } from './components/contact/contact.component';
     AboutComponent,
     ProjectsComponent,
     CreateComponent,
-    ContactComponent
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
