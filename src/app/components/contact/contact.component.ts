024df0c7ce72b8e42529032c604a24510020d9cb
Denis Rocher <denis.rocher@gmail.com>
2021-03-24 17:18:14 -0300

    --- Auto-Git Commit ---

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -11,18 +11,12 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    $("#logo").click(function (e: any) {
-      e.preventDefault();
-      $("header").css("background", "green")
-        .css("height", "150px");
-
-    });
-    $("#logo").dblclick(function (e: any) {
-      e.preventDefault();
-      $("header").css("background", "rgba(53, 103, 164, 0.8)")
-        .css("height", "98px");
-
+    $('.galeria').bxSlider({
+      auto: true,
+      autoControls: true,
+      stopAutoOnClick: true,
+      pager: true,
+      slideWidth: 800
     });
   }
-
 }
