0eddf142ea05b39e0a07ee5998a848faf887e757
Denis Rocher <denis.rocher@gmail.com>
2021-03-24 12:17:08 -0300

    --- Auto-Git Commit ---

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-declare var $: any;
+//declare var $: any;
 
 @Component({
   selector: 'app-contact',
@@ -11,11 +11,18 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    $("logo").click(function () {
+    $("#logo").click(function (e: any) {
+      e.preventDefault();
       $("header").css("background", "green")
-        .css("heigth", "50px");
+        .css("height", "150px");
 
-    })
+    });
+    $("#logo").dblclick(function (e: any) {
+      e.preventDefault();
+      $("header").css("background", "rgba(53, 103, 164, 0.8)")
+        .css("height", "98px");
+
+    });
   }
 
 }
